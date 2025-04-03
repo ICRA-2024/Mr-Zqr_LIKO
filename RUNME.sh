ef0cd5b4fbe1656cece43dddf02d63ccd9cd8f6c
Enric Cervera <ecervera@uji.es>
2025-04-03 11:44:02 +0200

    Add prerequisite checks

--- RUNME.sh
+++ RUNME.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+if ! curl -V &>/dev/null; then
+  echo "Prerequisite error: curl not found"
+  exit 1
+fi
+
+if ! docker -v &>/dev/null; then
+  echo "Prerequisite error: Docker not found"
+  exit 1
+fi
+
+if ! nvidia-container-toolkit --version &>/dev/null; then
+  echo "Prerequisite error: NVIDIA container toolkit not found"
+  exit 1
+fi
+
+if ! docker compose version &>/dev/null; then
+  echo "Prerequisite error: Docker compose plugin not found"
+  exit 1
+fi
+
 FILEID=1WIN043c4o_tXv2AdPYAQQ4RsoRk5dk3T
 FILENAME=square_walk.bag
 DATAFOLDER=dataset
